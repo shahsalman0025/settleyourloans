a9eb1ec1ac63918d7f449b7dbf188abe5d1e0785
Huzafa <huzaifatawab33@gmail.com>
2023-10-06 18:30:49 +0500

    salman

--- src/components/media.jsx
+++ src/components/media.jsx
@@ -7,9 +7,8 @@ import media4 from '../assets/img/media/media4.svg'
 function Media() {
   return (
     <div className="media_main">
-      <div className="feature__section-box  pb-40">
-        <h4 className="section-subtitle title-anim">You stay in control, we do the work for you.</h4>
-        <h3 className="section-title title-anim pt-20 pb-20">Media</h3>
+      <div className="feature__section-box text-center pb-40">
+        <h3 className="section-title title-anim pt-20 pb-20">In Media</h3>
       </div>
       <div className="media">
         <img src={media3} alt="" />
