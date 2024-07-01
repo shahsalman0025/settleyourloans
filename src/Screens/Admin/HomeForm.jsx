ab23c3423a8e17e1f5a2faae773522516a0937ba
Huzaifa Tawab <huzaifatawab33@gmail.com>
2024-07-01 13:49:30 +0500

    filter

--- src/Screens/Admin/HomeForm.jsx
+++ src/Screens/Admin/HomeForm.jsx
@@ -10,72 +10,100 @@ import ExportCSV from './ExportCSV';
 
 function HomeForm() {
   const [homeData, setHomeData] = useState([]);
+  const [fliteredHomeData, setFilteredHomeData] = useState([]);
   const [user, setUser] = useState(null);
   const [loader, setLoader] = useState(true);
 
   const [viewModal, setViewModal] = useState(false);
   const [viewModalId, setViewModalId] = useState();
 
+  const [fromDate, setFromDate] = useState("");
+  const [toDate, setToDate] = useState("");
+
   const auth = getAuth();
   useEffect(() => {
     getUserAuth();
-    FetchHomeFormData();
+    // FetchHomeFormData();
   }, []);
+  
   const getUserAuth = async () => {
     onAuthStateChanged(auth, (user) => {
       if (user === null) {
-        setUser(false)
-      }
-      else {
-        setUser(true)
+        setUser(false);
+      } else {
+        setUser(true);
       }
     });
-  }
+  };
+
   const FetchHomeFormData = async () => {
-    setLoader(true)
+    setLoader(true);
 
     await getDocs(collection(db, "homefromrecord"))
       .then((querySnapshot) => {
-        const newData = querySnapshot.docs
-          .map((doc) => ({ ...doc.data(), id: doc.id }));
+        const newData = querySnapshot.docs.map((doc) => ({ ...doc.data(), id: doc.id }));
         newData.sort((a, b) => new Date(b.date) - new Date(a.date));
-        setHomeData(newData)
-        setLoader(false)
-      })
-  }
+        setHomeData(newData);
+        setFilteredHomeData(newData)
+        setLoader(false);
+      });
+  };
+
+  const handleDateFilter = () => {
+    const from = new Date(fromDate);
+    const to = new Date(toDate);
+    const filteredData = homeData.filter((item) => {
+      const itemDate = new Date(item.date);
+      return itemDate >= from && itemDate <= to;
+    });
+    console.log(filteredData);
+    setFilteredHomeData(filteredData);
+  };
+
   const deleteBtnClick = async (e) => {
     e.preventDefault();
     const docRef = doc(db, "homefromrecord", e.target.id);
     await deleteDoc(docRef).then(() => {
-      alert("Deleted Sucessfully");
+      alert("Deleted Successfully");
       FetchHomeFormData();
     });
   };
-  // const viewBtnClick = async (e) => {
-  //   setViewModalId(e.target.id);
-  //   setViewModal(true);
-  // };
+
   return (
     <>
       {user ? (
         <>
-          {/* {viewModal && (
-            <ViewModal
-              setViewModal={setViewModal}
-              viewModalId={viewModalId && viewModalId}
-            />
-          )} */}
-
           <HeaderAdmin />
           <div className="max-w-7xl mx-auto sm:px-6 lg:px-8 pt-52">
-          <ExportCSV list={homeData} filename={"Home Form"}/>
+            <ExportCSV list={fliteredHomeData} filename={"Home Form"} />
+            
+            <div className="flex  mb-4">
+              <label className="mr-2">From:</label>
+              <input 
+                type="date" 
+                value={fromDate} 
+                onChange={(e) => setFromDate(e.target.value)} 
+                className="mr-4"
+              />
+              <label className="mr-2">To:</label>
+              <input 
+                type="date" 
+                value={toDate} 
+                onChange={(e) => setToDate(e.target.value)} 
+                className="mr-4"
+              />
+              <button 
+                onClick={handleDateFilter} 
+                className="bg-blue-500 text-white px-4 py-2 rounded"
+              >
+                Filter
+              </button>
+            </div>
 
             <div className="flex flex-col">
               <div className="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
                 <div className="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
                   <div className="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
-
-
                     <table className="min-w-full divide-y divide-gray-200">
                       <thead className="bg-gray-50">
                         <tr>
@@ -109,7 +137,6 @@ function HomeForm() {
                           >
                             Total Debt Amount
                           </th>
-
                           <th
                             scope="col"
                             className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
@@ -128,12 +155,6 @@ function HomeForm() {
                           >
                             Facing Issue
                           </th>
-                          {/* <th
-                            scope="col"
-                            className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
-                          >
-                            View
-                          </th> */}
                           <th
                             scope="col"
                             className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
@@ -143,17 +164,21 @@ function HomeForm() {
                         </tr>
                       </thead>
                       <tbody>
-                        {loader ? (<PELoader />) : (<>
-                          {homeData &&
-                            homeData.map((person, personIdx) => (
-                              <HomeFormData
-                                person={person}
-                                personIdx={personIdx}
-                                deleteBtnClick={deleteBtnClick}
-                              // viewBtnClick={viewBtnClick}
-                              />
-                            ))}
-                        </>)}
+                        {loader ? (
+                          <PELoader />
+                        ) : (
+                          <>
+                            {fliteredHomeData &&
+                              fliteredHomeData.map((person, personIdx) => (
+                                <HomeFormData
+                                  key={personIdx}
+                                  person={person}
+                                  personIdx={personIdx}
+                                  deleteBtnClick={deleteBtnClick}
+                                />
+                              ))}
+                          </>
+                        )}
                       </tbody>
                     </table>
                   </div>
@@ -161,77 +186,6 @@ function HomeForm() {
               </div>
             </div>
           </div>
-
-          {/* <section className="antialiased bg-gray-100 text-gray-600 px-4 h-fit">
-            <div className="flex flex-col justify-center h-full">
-              
-              <div className="w-full max-w-2xl mx-auto bg-white shadow-lg rounded-sm border border-gray-200">
-                <header className="px-5 py-4 border-b border-gray-100 flex ">
-                  <h2 className="font-semibold text-gray-800">Home Form's</h2>
-                </header>
-
-                <div className="p-3">
-                  <div className="overflow-x-auto">
-                    <table className="table-auto w-full">
-                      <thead className="text-xs font-semibold uppercase text-gray-400 bg-gray-50">
-                        <tr>
-                          <th className="p-2 whitespace-nowrap">
-                            <div className="font-semibold text-left">Name</div>
-                          </th>
-                          <th className="p-2 whitespace-nowrap">
-                            <div className="font-semibold text-left">
-                              Number
-                            </div>
-                          </th>
-                          <th className="p-2 whitespace-nowrap">
-                            <div className="font-semibold text-left">Email</div>
-                          </th>
-                          <th className="p-2 whitespace-nowrap">
-                            <div className="font-semibold text-left">
-                              Debt Amount
-                            </div>
-                          </th>
-                          <th className="p-2 whitespace-nowrap">
-                            <div className="font-semibold text-center">
-                              Harassment
-                            </div>
-                          </th>
-                        </tr>
-                      </thead>
-                      <tbody className="text-sm divide-y divide-gray-100">
-                        {homeData &&
-                          homeData.map((data) => (
-                            <tr>
-                              <td className="p-2 whitespace-nowrap">
-                                <div className="flex items-center">
-                                  <div className="font-medium text-gray-800">
-                                    {data.name}
-                                  </div>
-                                </div>
-                              </td>{" "}
-                              <td className="p-2 whitespace-nowrap">
-                                <div className="text-left">{data.number}</div>
-                              </td>
-                              <td className="p-2 whitespace-nowrap">
-                                <div className="text-left">{data.email}</div>
-                              </td>
-                              <td className="p-2 whitespace-nowrap">
-                                <div className="text-left font-medium text-green-500">
-                                  {data.debtAmount}
-                                </div>
-                              </td>
-                              <td className="p-2 whitespace-nowrap">
-                                <div className="text-lg text-center">??</div>
-                              </td>
-                            </tr>
-                          ))}
-                      </tbody>
-                    </table>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </section> */}
         </>
       ) : (
         <h1>Not Authorized</h1>
