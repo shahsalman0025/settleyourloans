00b3b22c3007120fe1e19c940b56f762eb4defa8
Huzaifa Tawab <huzaifatawab33@gmail.com>
2024-07-04 12:26:24 +0500

    pushhh

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -372,7 +372,7 @@ setNumberError("")
                         type="number"
                         id="address"
                         className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
-                        placeholder="PAN India"
+                        placeholder="Your City"
                         required
                         onChange={(e) => {
                           setAddress(e.target.value);
