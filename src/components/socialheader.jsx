939886c02c97e0d7fc5063a7d8ae02cebe4d08c0
Huzaifa-Tawab <huzaifatawab33@gmail.com>
2024-07-15 20:27:47 +0500

    #

--- src/components/socialheader.jsx
+++ src/components/socialheader.jsx
@@ -3,11 +3,12 @@ import googlePlay from '../assets/img/googleplay.png'
 function SocialHeader() {
     return (
         <div className="socialmain">
-
+{/* 
             <div className='comingsoon'>  <p>Now Available On </p> <button className='comingsoon-btn' onClick={()=>{
                     window.open('https://play.google.com/store/apps/details?id=com.settleLoans.app&pcampaignid=web_share', '_blank');
 
-            }}><img src={googlePlay} alt="" /></button></div>
+            }}><img src={googlePlay} alt="" /></button></div> */}
+            <div></div>
             <div className='header-social-icons'>
                 <a href="https://www.youtube.com/@settleloans/"><i className='fab fa-youtube'></i></a>
                 <a href="https://www.linkedin.com/company/settleloans/"><i className='fab fa-linkedin'></i></a>
